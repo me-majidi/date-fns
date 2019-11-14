0174d498c213251bdb1da25b278501d7a492c7cf
Mehran Majidi <me.majiidii@gmail.com>
2019-11-14 18:39:25 +0330

    Convert addMonths

--- src/addMonths/index.js
+++ src/addMonths/index.js
@@ -35,12 +35,12 @@ export default function addMonths(dirtyDate, dirtyAmount) {
   var date = toDate(dirtyDate)
   var amount = toInteger(dirtyAmount)
   var desiredMonth = date.getMonth() + amount
-  var dateWithDesiredMonth = new Date(0)
-  dateWithDesiredMonth.setFullYear(date.getFullYear(), desiredMonth, 1)
-  dateWithDesiredMonth.setHours(0, 0, 0, 0)
-  var daysInMonth = getDaysInMonth(dateWithDesiredMonth)
-  // Set the last day of the new month
-  // if the original date was the last day of the longer month
-  date.setMonth(desiredMonth, Math.min(daysInMonth, date.getDate()))
-  return new JDate(date)
+
+  var dateWithDesiredMonth = new JDate(date)
+  dateWithDesiredMonth.setFullYear(
+    date.getFullYear(),
+    desiredMonth,
+    date.getDate()
+  )
+  return new JDate(dateWithDesiredMonth)
 }
