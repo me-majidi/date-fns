1c46c3fb30f7d299cb95554d9bd7de914eb31892
Mehran Majidi <me.majiidii@gmail.com>
2019-11-15 14:01:18 +0330

    Update test functions for setDate method

--- src/jDate/test.js
+++ src/jDate/test.js
@@ -5,18 +5,59 @@ import assert from 'power-assert'
 import JDate from './index'
 
 describe('JDate', function() {
-  it('getFullYear', function() {
-    const date = new JDate(2015, 5, 23) // 2 Tir 1394
-    assert.equal(date.getFullYear(), 1394)
+  it('setDate: positive number smaller than one month', function() {
+    const date = new JDate(2019, 10 /* Nov */, 15) // 24 Aban 1398
+
+    date.setDate(1)
+    assert.equal(date.toLocaleDateString(), '1398/8/1')
   })
 
-  it('getMonth', function() {
-    const date = new JDate(2015, 5, 23) // 2 Tir 1394
-    assert.equal(date.getMonth(), 3)
+  it('setDate: positive number bigger than one month', function() {
+    const date = new JDate(2019, 10 /* Nov */, 15) // 24 Aban 1398
+
+    date.setDate(37)
+    assert.equal(date.toLocaleDateString(), '1398/9/7')
   })
 
-  it('getDate', function() {
-    const date = new JDate(2015, 5, 23) // 2 Tir 1394
-    assert.equal(date.getDate(), 2)
+  it('setDate: positive number bigger than two month', function() {
+    const date = new JDate(2019, 10 /* Nov */, 15) // 24 Aban 1398
+
+    date.setDate(83)
+    assert.equal(date.toLocaleDateString(), '1398/10/23')
+  })
+
+  it('setDate: positive number bigger that goes to next year', function() {
+    const date = new JDate(2019, 10 /* Nov */, 15) // 24 Aban 1398
+
+    date.setDate(152)
+    assert.equal(date.toLocaleDateString(), '1399/1/3')
+  })
+
+  it('setDate: pass 0 as date', function() {
+    const date = new JDate(2019, 10 /* Nov */, 15) // 24 Aban 1398
+
+    date.setDate(0)
+    assert.equal(date.toLocaleDateString(), '1398/7/30')
+  })
+
+  it('setDate: pass negative number', function() {
+    const date = new JDate(2019, 10 /* Nov */, 15) // 24 Aban 1398
+
+    date.setDate(-2)
+    assert.equal(date.toLocaleDateString(), '1398/7/29')
+  })
+
+  it('setDate: pass negative number', function() {
+    const date = new JDate(2019, 10 /* Nov */, 15) // 24 Aban 1398
+
+    date.setDate(-30)
+    assert.equal(date.toLocaleDateString(), '1398/7/1')
+  })
+
+  it('setDate: pass negative number which goes 2months ago', function() {
+    const date = new JDate(2019, 10 /* Nov */, 15) // 24 Aban 1398
+
+    date.setDate(-95)
+    assert.equal(date.toLocaleDateString(), '1398/4/29')
   })
 })
