c38a13015c42442ee14710975589d618eb09e0e8
Mehran Majidi <m.majidi@digikala.com>
2019-11-16 13:09:46 +0330

    Update test files config

--- test.js
+++ test.js
@@ -1,2 +1,12 @@
-var testsContext = require.context('./src/', true, /\/test\.js$/)
-testsContext.keys().forEach(testsContext)
+var testsContext = require.context('./src', true, /\/test\.js$/)
+
+var convertedFunctions = ['jDate', 'addDays']
+
+testsContext
+  .keys()
+  .filter(r => {
+    var methodName = r.split('/').filter(s => s !== '.')[0]
+
+    return convertedFunctions.indexOf(methodName) !== -1
+  })
+  .forEach(testsContext)
