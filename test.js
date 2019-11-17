c2dc2c77a6032eaf2672a2baf6266eb91942375b
Mehran Majidi <me.majiidii@gmail.com>
2019-11-17 18:08:16 +0330

    Update karma test files configuration

--- test.js
+++ test.js
@@ -1,2 +1,11 @@
 var testsContext = require.context('./src/', true, /\/test\.js$/)
-testsContext.keys().forEach(testsContext)
+
+const convertedMethods = ['jDate']
+
+testsContext
+  .keys()
+  .filter(r => {
+    const filename = r.split('/').filter(s => s !== '.')[0]
+    return convertedMethods.indexOf(filename) !== -1
+  })
+  .forEach(testsContext)
