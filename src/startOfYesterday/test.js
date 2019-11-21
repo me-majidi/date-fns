02f660e360fd6b18e52fa31e5f6f6a55307996e4
Mehran Majidi <me.majiidii@gmail.com>
2019-11-21 20:06:38 +0330

    Convert startOfYesterday

--- src/startOfYesterday/test.js
+++ src/startOfYesterday/test.js
@@ -4,30 +4,19 @@
 import assert from 'assert'
 import sinon from 'sinon'
 import startOfYesterday from '.'
+import JDate from '../jDate'
 
 describe('startOfYesterday', function() {
   it('returns the start of yesterday', function() {
     const clock = sinon.useFakeTimers(
-      new Date(2014, 8 /* Sep */, 25, 14, 30, 45, 500).getTime()
+      new JDate({ year: 1398, month: 8 /* Azar */, day: 22 }).getTime()
     )
 
     const result = startOfYesterday()
-    assert.deepEqual(result, new Date(2014, 8 /* Sep */, 24))
-
-    clock.restore()
-  })
-
-  it('handles dates before 100 AD', function() {
-    const now = new Date(0)
-    now.setFullYear(14, 8 /* Sep */, 25)
-    now.setHours(0, 0, 0, 0)
-    const clock = sinon.useFakeTimers(now.getTime())
-
-    const expectedResult = new Date(0)
-    expectedResult.setFullYear(14, 8 /* Sep */, 24)
-    expectedResult.setHours(0, 0, 0, 0)
-    const result = startOfYesterday()
-    assert.deepEqual(result, expectedResult)
+    assert.deepEqual(
+      result,
+      new JDate({ year: 1398, month: 8 /* Azar */, day: 21 })
+    )
 
     clock.restore()
   })
--- test.js
+++ test.js
@@ -16,6 +16,7 @@ const convertedMethods = [
   'lastDayOfWeek',
   'lastDayOfYear',
   // 'areIntervalsOverlapping',
+  'startOfYesterday',
   'subSeconds',
   'subWeeks',
   'subYears',
