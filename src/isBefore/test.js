8d0561e2c372454b017b8afb818e316b7a06ac9f
Mehran Majidi <me.majiidii@gmail.com>
2019-11-23 00:07:26 +0330

    Convert isBefore

--- src/isBefore/test.js
+++ src/isBefore/test.js
@@ -3,52 +3,59 @@
 
 import assert from 'power-assert'
 import isBefore from '.'
+import JDate from '../jDate'
 
 describe('isBefore', function() {
   it('returns true if the first date is before the second one', function() {
     var result = isBefore(
-      new Date(1987, 1 /* Feb */, 11),
-      new Date(1989, 6 /* Jul */, 10)
+      new JDate({ year: 1391, month: 8 /* Azar */, day: 8 }),
+      new JDate({ year: 1394, month: 5 /* Tir */, day: 21 })
     )
     assert(result === true)
   })
 
   it('returns false if the first date is after the second one', function() {
     var result = isBefore(
-      new Date(1989, 6 /* Jul */, 10),
-      new Date(1987, 1 /* Feb */, 11)
+      new JDate({ year: 1398, month: 8 /* Azar */, day: 15 }),
+      new JDate({ year: 1394, month: 3 /* Tir */, day: 21 })
     )
     assert(result === false)
   })
 
   it('returns false if the first date is equal to the second one', function() {
     var result = isBefore(
-      new Date(1989, 6 /* Jul */, 10),
-      new Date(1989, 6 /* Jul */, 10)
+      new JDate({ year: 1394, month: 3 /* Tir */, day: 21 }),
+      new JDate({ year: 1394, month: 3 /* Tir */, day: 21 })
     )
     assert(result === false)
   })
 
   it('accepts a timestamp', function() {
     var result = isBefore(
-      new Date(1987, 1 /* Feb */, 11).getTime(),
-      new Date(1989, 6 /* Jul */, 10).getTime()
+      new JDate({ year: 1394, month: 3 /* Tir */, day: 21 }).getTime(),
+      new JDate({ year: 1394, month: 5 /* Shahrivar */, day: 3 }).getTime()
     )
     assert(result === true)
   })
 
   it('returns false if the first date is `Invalid Date`', function() {
-    var result = isBefore(new Date(NaN), new Date(1989, 6 /* Jul */, 10))
+    var result = isBefore(
+      new JDate(NaN),
+      new JDate({ year: 1394, month: 5 /* Shahrivar */, day: 3 })
+    )
     assert(result === false)
   })
 
   it('returns false if the second date is `Invalid Date`', function() {
-    var result = isBefore(new Date(1987, 1 /* Feb */, 11), new Date(NaN))
+    var result = isBefore(
+      new JDate({ year: 1394, month: 5 /* Shahrivar */, day: 3 }),
+      new JDate(NaN)
+    )
     assert(result === false)
   })
 
   it('returns false if the both dates are `Invalid Date`', function() {
-    var result = isBefore(new Date(NaN), new Date(NaN))
+    var result = isBefore(new JDate(NaN), new JDate(NaN))
     assert(result === false)
   })
 
--- test.js
+++ test.js
@@ -32,6 +32,7 @@ const convertedMethods = [
   'getSeconds',
   'getTime',
   'isAfter',
+  'isBefore',
   'isLeapYear',
   'isWeekend',
   'setYear',
