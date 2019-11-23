7b2d3420033bd16b95ddff89589c81eeee118c88
MohammadReza <herbodabdoli@gmail.com>
2019-11-23 17:25:40 +0330

    add milisecond test

--- src/addMilliseconds/test.js
+++ src/addMilliseconds/test.js
@@ -3,58 +3,155 @@
 
 import assert from 'power-assert'
 import addMilliseconds from '.'
+import JDate from '../jDate'
 
 describe('addMilliseconds', function() {
   it('adds the given number of milliseconds', function() {
-    var result = addMilliseconds(
-      new Date(2014, 6 /* Jul */, 10, 12, 45, 30, 0),
-      750
+    var date = new JDate({
+      year: 1398,
+      month: 8 /* Azar */,
+      day: 2,
+      hours: 12,
+      min: 45,
+      sec: 30,
+      ms: 0
+    })
+    var result = addMilliseconds(date, 750)
+    assert.deepEqual(
+      result,
+      new JDate({
+        year: 1398,
+        month: 8 /* Azar */,
+        day: 2,
+        hours: 12,
+        min: 45,
+        sec: 30,
+        ms: 750
+      })
     )
-    assert.deepEqual(result, new Date(2014, 6 /* Jul */, 10, 12, 45, 30, 750))
   })
 
   it('accepts a timestamp', function() {
-    var result = addMilliseconds(
-      new Date(2014, 6 /* Jul */, 10, 12, 45, 30, 0).getTime(),
-      500
+    var date = new JDate({
+      year: 1398,
+      month: 8 /* Azar */,
+      day: 2,
+      hours: 12,
+      min: 45,
+      sec: 30,
+      ms: 0
+    })
+    var result = addMilliseconds(date, 500)
+    assert.deepEqual(
+      result,
+      new JDate({
+        year: 1398,
+        month: 8 /* Azar */,
+        day: 2,
+        hours: 12,
+        min: 45,
+        sec: 30,
+        ms: 500
+      })
     )
-    assert.deepEqual(result, new Date(2014, 6 /* Jul */, 10, 12, 45, 30, 500))
   })
 
   it('converts a fractional number to an integer', function() {
-    var result = addMilliseconds(
-      new Date(2014, 6 /* Jul */, 10, 12, 45, 30, 0),
-      750.75
+    var date = new JDate({
+      year: 1398,
+      month: 8 /* Azar */,
+      day: 2,
+      hours: 12,
+      min: 45,
+      sec: 30,
+      ms: 0
+    })
+    var result = addMilliseconds(date, 750.75)
+    assert.deepEqual(
+      result,
+      new JDate({
+        year: 1398,
+        month: 8 /* Azar */,
+        day: 2,
+        hours: 12,
+        min: 45,
+        sec: 30,
+        ms: 750
+      })
     )
-    assert.deepEqual(result, new Date(2014, 6 /* Jul */, 10, 12, 45, 30, 750))
   })
 
   it('implicitly converts number arguments', function() {
+    var date = new JDate({
+      year: 1398,
+      month: 8 /* Azar */,
+      day: 2,
+      hours: 12,
+      min: 45,
+      sec: 30,
+      ms: 5
+    })
     var result = addMilliseconds(
-      new Date(2014, 6 /* Jul */, 10, 12, 45, 30, 5),
+      date,
       // $ExpectedMistake
       '750'
     )
-    assert.deepEqual(result, new Date(2014, 6 /* Jul */, 10, 12, 45, 30, 755))
+    assert.deepEqual(
+      result,
+      new JDate({
+        year: 1398,
+        month: 8 /* Azar */,
+        day: 2,
+        hours: 12,
+        min: 45,
+        sec: 30,
+        ms: 755
+      })
+    )
   })
 
   it('does not mutate the original date', function() {
-    var date = new Date(2014, 6 /* Jul */, 10, 12, 45, 30, 0)
+    var date = new JDate({
+      year: 1398,
+      month: 8 /* Azar */,
+      day: 2,
+      hours: 12,
+      min: 45,
+      sec: 30,
+      ms: 0
+    })
     addMilliseconds(date, 250)
-    assert.deepEqual(date, new Date(2014, 6 /* Jul */, 10, 12, 45, 30, 0))
+    assert.deepEqual(
+      date,
+      new JDate({
+        year: 1398,
+        month: 8 /* Azar */,
+        day: 2,
+        hours: 12,
+        min: 45,
+        sec: 30,
+        ms: 0
+      })
+    )
   })
 
   it('returns `Invalid Date` if the given date is invalid', function() {
-    var result = addMilliseconds(new Date(NaN), 750)
-    assert(result instanceof Date && isNaN(result))
+    var result = addMilliseconds(new JDate(NaN), 750)
+    assert(result instanceof JDate && isNaN(result))
   })
 
   it('returns `Invalid Date` if the given amount is NaN', function() {
-    var result = addMilliseconds(
-      new Date(2014, 6 /* Jul */, 10, 12, 45, 30, 0),
-      NaN
-    )
-    assert(result instanceof Date && isNaN(result))
+    var date = new JDate({
+      year: 1398,
+      month: 8 /* Azar */,
+      day: 2,
+      hours: 12,
+      min: 45,
+      sec: 30,
+      ms: 0
+    })
+    var result = addMilliseconds(date, NaN)
+    assert(result instanceof JDate && isNaN(result))
   })
 
   it('throws TypeError exception if passed less than 2 arguments', function() {
